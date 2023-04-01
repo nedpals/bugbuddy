e73c9e330b8992ec2d8378f1209ea3672e26fa68
Ned Palacios <npdoesmc@gmail.com>
2023-04-01 12:51:22 +0800

    fix: improve lsp server connection handling

--- server/lsp_server.go
+++ server/lsp_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"os"
 
 	"github.com/sourcegraph/jsonrpc2"
@@ -32,14 +33,38 @@ func (s *LspServer) Handle(ctx context.Context, c *jsonrpc2.Conn, r *jsonrpc2.Re
 			},
 		})
 	case "initialized":
-		c.Reply(ctx, r.ID, map[string]string{})
+		c.Notify(ctx, lsp.MethodWindowShowMessage, lsp.ShowMessageParams{
+			Type:    lsp.MessageTypeInfo,
+			Message: "Client is connected",
+		})
 	case "shutdown":
 		c.Reply(ctx, r.ID, json.RawMessage("null"))
 		return
 	case "exit":
 		<-s.doneChan
-		return
 	}
+}
+
+type connection struct {
+	io.ReadCloser
+	io.WriteCloser
+}
+
+func (conn *connection) Read(p []byte) (n int, err error) {
+	return conn.ReadCloser.Read(p)
+}
+
+func (conn *connection) Write(p []byte) (n int, err error) {
+	return conn.WriteCloser.Write(p)
+}
+
+func (conn *connection) Close() error {
+	if err := conn.ReadCloser.Close(); err != nil {
+		return err
+	} else if err := conn.WriteCloser.Close(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func startLspServer(addr string) error {
@@ -48,38 +73,57 @@ func startLspServer(addr string) error {
 		publishChan:            make(chan int),
 	}
 
+	lspServer.conn = jsonrpc2.NewConn(
+		context.Background(),
+		jsonrpc2.NewBufferedStream(&connection{
+			ReadCloser:  os.Stdin,
+			WriteCloser: os.Stdout,
+		}, jsonrpc2.VSCodeObjectCodec{}),
+		jsonrpc2.AsyncHandler(lspServer),
+	)
+
 	lspServer.daemonClient = connectToDaemon(DEFAULT_DAEMON_PORT, CLIENT_TYPE_LSP, func(ctx context.Context, c *jsonrpc2.Conn, r *jsonrpc2.Request) {
+		lspServer.conn.Notify(ctx, lsp.MethodWindowShowMessage, lsp.ShowMessageParams{
+			Type:    lsp.MessageTypeInfo,
+			Message: r.Method,
+		})
+
 		if r.Notif && r.Method == "clients/report" {
 			lspServer.unpublishedDiagnostics = append(lspServer.unpublishedDiagnostics, "test")
 			lspServer.publishChan <- len(lspServer.unpublishedDiagnostics)
 		}
 	})
 
+	ctx := context.Background()
+	lspServer.daemonClient.OnReconnect = func() {
+		lspServer.conn.Notify(ctx, lsp.MethodWindowShowMessage, lsp.ShowMessageParams{
+			Type:    lsp.MessageTypeInfo,
+			Message: "Daemon not connected. Launching...",
+		})
+	}
+
 	if err := lspServer.daemonClient.EnsureConnection(); err != nil {
 		return err
 	}
 
-	lspServer.conn = jsonrpc2.NewConn(
-		context.Background(),
-		jsonrpc2.NewBufferedStream(os.Stdin, jsonrpc2.VSCodeObjectCodec{}),
-		jsonrpc2.AsyncHandler(lspServer),
-	)
-
-	ctx := context.Background()
-
-	defer func() {
-		lspServer.daemonClient.Close()
-		lspServer.conn.Close()
-	}()
-
 	for {
 		select {
 		case <-lspServer.publishChan:
+			diagnostics := make([]lsp.Diagnostic, len(lspServer.unpublishedDiagnostics))
+			for n, errMsg := range lspServer.unpublishedDiagnostics {
+				diagnostics[n] = lsp.Diagnostic{
+					Severity: lsp.DiagnosticSeverityError,
+					Message:  errMsg,
+				}
+			}
+
 			lspServer.conn.Notify(ctx, lsp.MethodTextDocumentPublishDiagnostics, lsp.PublishDiagnosticsParams{
-				// TODO:
+				Diagnostics: diagnostics,
 			})
 		case <-lspServer.doneChan:
-			return nil
+			lspServer.daemonClient.Close()
+			lspServer.conn.Close()
+			os.Exit(0)
 		}
 	}
 }
