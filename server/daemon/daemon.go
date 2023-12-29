be42a5b41273046ddabd4cc1c7c4d7311b36298a
Ned Palacios <npdoesmc@gmail.com>
2023-12-29 18:43:19 +0800

    feat(daemon): configure default daemon port in client

--- server/daemon/daemon.go
+++ server/daemon/daemon.go
@@ -2,6 +2,7 @@ package daemon
 
 import (
 	"context"
+	"os"
 
 	"github.com/nedpals/bugbuddy/server/daemon/client"
 	"github.com/nedpals/bugbuddy/server/daemon/server"
@@ -9,7 +10,16 @@ import (
 	"github.com/nedpals/bugbuddy/server/rpc"
 )
 
+
 const DEFAULT_PORT = ":3434"
+
+var CURRENT_DAEMON_PORT = DEFAULT_PORT
+
+func init() {
+	if port := os.Getenv("BUGBUDDY_DAEMON_PORT"); len(port) > 0 {
+		CURRENT_DAEMON_PORT = ":" + port
+	}
+}
 
 func NewClient(ctx context.Context, addr string, clientType types.ClientType, handlerFunc ...rpc.HandlerFunc) *client.Client {
 	return client.NewClient(ctx, addr, clientType, handlerFunc...)
