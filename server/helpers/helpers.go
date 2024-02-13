4da6d59f8a14e133e19776e9add645d13de3cf07
norg-ned <ned@norg.ai>
2024-02-13 17:53:42 +0800

    feat(helpers): add ability to set dir path via env or manually

--- server/helpers/helpers.go
+++ server/helpers/helpers.go
@@ -5,7 +5,26 @@ import (
 	"path/filepath"
 )
 
+var bbDirPath = ""
+
+func SetDirPath(newPath string) error {
+	// NOTE: when path does not exist, use GetOrInitializeDir
+
+	// set the new path
+	bbDirPath = newPath
+	return nil
+}
+
 func GetDirPath() string {
+	// check if BUGBUDDY_DIR is set. if present, set it as the directory path
+	if envPath := os.Getenv("BUGBUDDY_DIR"); len(envPath) != 0 && bbDirPath != envPath {
+		SetDirPath(envPath)
+	}
+
+	if len(bbDirPath) != 0 {
+		return bbDirPath
+	}
+
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		if homeEnv := os.Getenv("HOME"); len(homeEnv) != 0 {
